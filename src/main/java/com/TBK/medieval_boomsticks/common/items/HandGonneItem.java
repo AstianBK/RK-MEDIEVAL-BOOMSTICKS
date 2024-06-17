5ab62f0ab46274967ce4b132554a8468ff3cc6c8
AstianBK <Agustinmarin2016@gmail.com>
2024-06-16 21:31:41 -0300

    Se agrego la posibilidad que falle el disparo.

--- src/main/java/com/TBK/medieval_boomsticks/common/items/HandGonneItem.java
+++ src/main/java/com/TBK/medieval_boomsticks/common/items/HandGonneItem.java
@@ -2,7 +2,13 @@ package com.TBK.medieval_boomsticks.common.items;
 
 import com.TBK.medieval_boomsticks.client.renderer.HandGonneRenderers;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.core.object.PlayState;
 
 import java.util.function.Consumer;
 
@@ -22,5 +28,18 @@ public class HandGonneItem extends FireGunItem {
                 return renderer;
             }
         });
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController(this,"controller",0,e->{
+            ItemStack stack= (ItemStack) e.getData(DataTickets.ITEMSTACK);
+            if (isReCharge(stack)) {
+                e.getController().setAnimation(RawAnimation.begin().thenPlayAndHold("handgonne.shoot"));
+            }else{
+                e.getController().setAnimation(RawAnimation.begin().thenLoop("handgonne.idle"));
+            }
+            return PlayState.CONTINUE;
+        }));
     }
 }
